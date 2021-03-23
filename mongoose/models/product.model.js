7a940eb26f5c975451a14cadae7f99b0a7419be9
Sayantan Samanta <sssamanta789@gmail.com>
2021-03-23 21:47:09 +0530

    Added product schema

---
+++ mongoose/models/product.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema
+
+const productSchema  = new Schema({
+    name : {
+        type : String,
+        required : true 
+    },
+    tagname : {
+        type : String,
+        required: true
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    image : {
+        type : String,
+        required : true
+    },
+    price : {
+        type : Number,
+        required : true
+    },
+    gender : {
+        type : Boolean,
+        required : true
+    }
+})
+
+module.exports = mongoose.model('Product', productSchema)
