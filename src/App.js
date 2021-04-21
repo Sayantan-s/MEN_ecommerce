19acfaf47c98a36e02a395df791b56e2b3abd53b
Sayantan Samanta <sssamanta789@gmail.com>
2021-04-22 00:51:39 +0530

    Added intial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
 function App() {
+
+  useEffect(() => {
+    const body =  document.querySelector('body');
+    body.children[1].className = "overflow-x-hidden bg-red-400";
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="overflow-x-hidden">
+      Hello!!
     </div>
   );
 }
